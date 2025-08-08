ecc27a0b8d0c6f09e0e77deb31370c5dde40bb48
DM!TRY <adam.lean.92@gmail.com>
2025-08-08 22:43:49 +0800

    Update about.jsx

--- src/page/about.jsx
+++ src/page/about.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// Компоненты
+// Компоненты /
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import SubscribeSection from "../components/SubscribeSection";
